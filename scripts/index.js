dd4c0bb1608f55ba723bd0ab2eddf630d6298399
andrei.antonov <andrei.antonov@corp.mail.ru>
2024-07-03 20:32:19 +0300

    Рабочий вариант

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,42 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+
 
 // @todo: DOM узлы
+const content = document.querySelector('.content');
+const placesList = document.querySelector('.places__list');
+
 
 // @todo: Функция создания карточки
+function createCard(element, remove) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+    cardElement.querySelector('.card__title').textContent = element.name;
+
+    const cardImage = cardElement.querySelector('.card__image');
+
+    cardImage.src = element.link;
+    cardImage.alt = element.name;
+
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', remove);
+
+    return cardElement;
+}
+
 
 // @todo: Функция удаления карточки
+function removeCard(event) {
+    const card = event.target.closest('.card');
+    card.remove();
+}
+
 
 // @todo: Вывести карточки на страницу
+function renderCards() {
+    initialCards.forEach((element) => {
+        placesList.append(createCard(element, removeCard));
+    });
+}
+
+renderCards();
